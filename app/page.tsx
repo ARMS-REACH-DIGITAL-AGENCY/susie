ff5af885f88b09729777b399636d7d5a63e29d12
YAT?STATS <pcdaction@gmail.com>
2026-07-26 23:55:03 -0700

    Remove duplicate homepage quiz form

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Problem from "@/components/Problem";
 import Services from "@/components/Services";
-import QuizForm from "@/components/QuizForm";
 import Trust from "@/components/Trust";
 import FAQ from "@/components/FAQ";
 import Footer from "@/components/Footer";
@@ -15,7 +14,6 @@ export default function Home() {
         <Hero />
         <Problem />
         <Services />
-        <QuizForm />
         <Trust />
         <FAQ />
       </main>
